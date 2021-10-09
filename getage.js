b50781422d663aa03dbddb73011724a9ad9c04b4
kheynov <lcastory86@gmail.com>
2021-10-09 14:55:05 +0700

    initial commit

---
+++ getage.js
@@ -0,0 +1,14 @@
+var now = new Date(); 
+var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); 
+var dob = new Date(2002, 12, 17);
+var dobCurrentYear = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()); 
+var age; 
+
+//Возраст = текущий год - год рождения
+age = today.getFullYear() - dob.getFullYear();
+//Если ДР в этом году ещё предстоит, то вычитаем из age один год
+if (today < dobCurrentYear) {
+    age = age - 1;
+}
+
+document.getElementById("age").innerHTML = age + " лет &nbsp;&bull;&nbsp; Android разработчик &nbsp;&bull;&nbsp; Студент"
